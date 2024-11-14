963336eb13016e6ef6047d07786bbe84d6f79810
Greedeks <effect.adqw@gmail.com>
2024-11-14 10:51:31 +0300

    -changed title

--- .Source/GTweak/Properties/AssemblyInfo.cs
+++ .Source/GTweak/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("Customize your Windows")]
+[assembly: AssemblyTitle("GTweak")]
 [assembly: AssemblyDescription("Perfect setup of Windows 10 & Windows 11 with the choice of options you need for better convenience and performance")]
 [assembly: AssemblyProduct("GTweak")]
 [assembly: AssemblyCopyright("Copyright © Greedeks 2024")]
 [assembly: AssemblyTrademark("Greedeks")]
 [assembly: AssemblyInformationalVersion("Build: 5.0.0")]
-[assembly: AssemblyVersion("5.0.27.572")]
-[assembly: AssemblyFileVersion("5.0.27.572")]
+[assembly: AssemblyVersion("5.0.28.246")]
+[assembly: AssemblyFileVersion("5.0.28.246")]
 
 [assembly: ComVisible(false)]
 
