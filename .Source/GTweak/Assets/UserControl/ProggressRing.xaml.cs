e8e531768d8b0c1298d916011a76463bc55a5a9b
Greedeks <effect.adqw@gmail.com>
2024-11-16 12:41:40 +0300

    -progreess ring added in uc

---
+++ .Source/GTweak/Assets/UserControl/ProggressRing.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+
+namespace GTweak.Assets.UserControl
+{
+    public partial class ProggressRing
+    {
+        internal Brush ChangeBackground  { get => Back.Stroke; set { Back.Stroke = value; } }
+        internal double ChangeStrokeThickness { get => Back.StrokeThickness; set { Main.StrokeThickness = Back.StrokeThickness = value; } }
+        public ProggressRing()
+        {
+            InitializeComponent();
+        }
+    }
+}
