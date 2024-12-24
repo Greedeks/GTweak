4b7408873c7cf534fc4b7ac979f63b1df906d0d6
Greedeks <effect.adqw@gmail.com>
2024-12-24 17:40:59 +0300

    v 5.0.5

--- .Source/GTweak/Properties/AssemblyInfo.cs
+++ .Source/GTweak/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Windows;
 [assembly: AssemblyProduct("GTweak")]
 [assembly: AssemblyCopyright("Copyright © Greedeks 2024")]
 [assembly: AssemblyTrademark("Greedeks")]
-[assembly: AssemblyInformationalVersion("Build: 5.0.4")]
-[assembly: AssemblyVersion("5.0.38.18")]
-[assembly: AssemblyFileVersion("5.0.38.18")]
+[assembly: AssemblyInformationalVersion("Build: 5.0.5")]
+[assembly: AssemblyVersion("5.0.52.843")]
+[assembly: AssemblyFileVersion("5.0.52.843")]
 
 [assembly: ComVisible(false)]
 
